2f241571c965068c1b2e005311aa4b76f9790965
sam kim <seungkim11@gmail.com>
2016-04-12 18:18:05 -0700

    ready to deploy in aws

--- twitter/twitterAPI.py
+++ twitter/twitterAPI.py
@@ -3,6 +3,7 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import time
+from pymongo import MongoClient
 
 with open('auth.json') as auth_file:
     auth_data = json.load(auth_file)
@@ -12,15 +13,24 @@ csecret = auth_data['csecret']
 atoken = auth_data['atoken']
 asecret = auth_data['asecret']
 
+client = MongoClient('localhost', 27017)
+db = client['election-2016']
+tweets_test = db['tweets_test']
+
+dbfile = open('data.json', 'w')
+dbfile.write('[\n')
+dbfile.close()
+
 class listener(StreamListener):
     def on_data(self, data):
         try:
-            print data
-            dbfile = open('tweetDB.json', 'a')
-            dbfile.write(data)
-            dbfile.write('\n')
-            dbfile.close()
+            tweet = json.loads(data)
+            # with open('data.json', 'a') as outfile:
+            #     json.dump(tweet, outfile, sort_keys=True, indent=4)
+            #     outfile.write(',\n')
+            tweets_test.insert_one(tweet)
             return True
+
         except BaseException, e:
             print 'failed ondata, ' , str(e)
             time.sleep(5)
@@ -32,5 +42,5 @@ auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=['election2016', 'debate', 'bernie sanders', 'hilary clinton', 'trump', 'ted cruz'])
+twitterStream.filter(track=['2016 election', 'bernie sanders', 'hilary clinton', 'trump', 'ted cruz', 'democratic party', 'republican party'])
 
