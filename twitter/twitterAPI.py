d66d7194199ddaad86240577bda6c98993474670
sam kim <seungkim11@gmail.com>
2016-04-14 02:01:49 -0700

    changed to python3 for to solve disconnect issue

--- twitter/twitterAPI.py
+++ twitter/twitterAPI.py
@@ -18,48 +18,33 @@ client = MongoClient('localhost', 27017)
 db = client['election-2016']
 tweets_test = db['tweets_test']
 
-dbfile = open('data.json', 'w')
-dbfile.write('[\n')
-dbfile.close()
+class StdOutListener(StreamListener):
 
-
-class listener(StreamListener, api=None):
     def __init__(self):
-        super(listener, self).__init__()
-        self.switch = True
+        self.switcher = 1
 
-    def on_status(self, data):
-        try:
-            if (self.switch == True):
-                self.switch = False
-                tweet = json.loads(data)
-                with open('data.json', 'a') as outfile:
-                     json.dump(tweet, outfile, sort_keys=True, indent=4)
-                     outfile.write(',\n')
-                tweets_test.insert_one(tweet)
-                print tweet
+    def on_data(self, data):
+        if (self.switcher == 1):
 
-                return True
-            else:
-                self.switch = True
+            tweet = json.loads(data)
+            print(tweet['id_str'])
+            tweets_test.insert_one(tweet)
+            self.switcher = 0
+            return True
 
-        except BaseException, e:
-            print 'failed ondata, ' , str(e)
-            time.sleep(5)
+        else:
+            self.switcher = 1
+            print('rest')
+            return True
 
-    def on_error(self, status_code):
-        print status_code
+    def on_error(self, status):
+        print(status)
 
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
 
-while True:
-    try:
-        twitterStream = Stream(auth, listener())
-        twitterStream.filter(track=['2016 election', 'bernie sanders', 'hilary clinton', 'trump', 'ted cruz', 'democratic party', 'republican party'])
-    except ProtocolError:
-        time.sleep(5)
-        continue
-    except KeyboardInterrupt:
-        twitterStream.disconnect()
-        break
+    stream = Stream(auth, l)
+    stream.filter(track=['2016 election', 'bernie sanders', 'hilary clinton', 'trump', 'ted cruz',
+                         'democratic party','republican party'])
